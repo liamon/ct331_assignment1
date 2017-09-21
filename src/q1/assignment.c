0b0c4ebf6a105a8feeb9581dd589c060a4c27b95
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-09-21 15:37:48 +0100

    Question 1 code.

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+  int integer = 23;
+  int* integerPointer = &integer;
+  long longInteger = 2452;
+  double* doublePointer;
+  char** pointerToCharPointer;
+
+  printf("Size of int = %zu\n", sizeof(integer));
+  printf("Size of int* = %zu\n", sizeof(integerPointer));
+  printf("Size of long = %zu\n", sizeof(longInteger));
+  printf("Size of double* = %zu\n", sizeof(doublePointer));
+  printf("Size of char** = %zu\n", sizeof(pointerToCharPointer));
   return 0;
 }
