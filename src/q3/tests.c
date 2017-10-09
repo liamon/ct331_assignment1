f41e4bf2f4ba622ce94bc952486f1fae785ea017
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-09 22:55:42 +0100

    Changed testGenericness() to use push().

--- src/q3/tests.c
+++ src/q3/tests.c
@@ -26,29 +26,29 @@ void testGenericness() {
   
   char plusSign = '+';
   printf("Expected: %c\n", plusSign);
-  genericListElement* element = createEl(&plusSign, sizeof(char), &printChar);
+  genericListElement* list = createEl(&plusSign, sizeof(char), &printChar);
   printf("Actual: ");
-  element->print(element->data);
+  list->print(list->data);
 
   int integer = 4845;
   printf("Expected: %d\n", integer);
-  element = createEl(&integer, sizeof(int), &printInt);
+  push(&list, &integer, sizeof(int), &printInt);
   printf("Actual: ");
-  element->print(element->data);
+  list->print(list->data);
 
   double floatingPoint = 192.375;
   printf("Expected: %lf\n", floatingPoint);
-  element = createEl(&floatingPoint, sizeof(double), &printDouble);
+  push(&list, &floatingPoint, sizeof(double), &printDouble);
   printf("Actual: ");
-  element->print(element->data);
+  list->print(list->data);
 
   char* string = "This is a test.";
   printf("Expected: %s\n", string);
   // sizeof(string) will just return the size of the pointer/address, so I have
   // to use strlen. strlen ignores '\0', so I have to + 1 to include it.
-  element = createEl(&string, strlen(string) + 1, &printString);
+  push(&list, &string, strlen(string) + 1, &printString);
   printf("Actual: ");
-  element->print(element->data);
+  list->print(list->data);
 }
 
 void printInt(void* intToPrint) {
